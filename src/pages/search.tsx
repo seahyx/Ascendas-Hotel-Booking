2bd445c7a70ec62ba1d2caf300a317a9554a6aca
Kendrick <kendricknghy@gmail.com>
2023-07-22 14:52:00 +0800

    Added details to the filter sidebar

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,4 +1,4 @@
-import { Box, Slider, Container, Grid, Rating, Stack, Typography, TextField } from "@mui/material";
+import { Checkbox, Box, Slider, Container, Grid, Rating, Stack, Typography, TextField, FormGroup, FormControlLabel } from "@mui/material";
 import Head from "next/head";
 import TopBarWithSearch from "~/components/TopBarWithSearch";
 
@@ -7,18 +7,23 @@ function Sidebar() {
     <Stack className="pt-1 w-72" spacing={2}>
       <Box className="h-64" bgcolor="secondary.light"></Box>
       <Box className="relative h-[32rem] shadow-xl" bgcolor="info.main">
-        <title>Test</title>
-        <Container className="w-2/3 object-center">
-          <Typography component="legend">Rating1</Typography>
-          <Rating name="rating1" value={2}/>
-        </Container>
-        <Container className="mt-4 w-2/3 object-center">
-          <Typography component="legend">Rating2</Typography>
-          <Rating name="rating2" value={3}/>
+        <Typography className="pl-4 pt-2">Filters</Typography>
+        <Container className="mt-4 object-center">
+          <TextField size="small"></TextField>
+          <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
         </Container>
         <Container className="mt-4 object-center">
           <TextField size="small"></TextField>
           <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
+        </Container>
+        <Container className="w-2/3 object-center">
+          <FormGroup>
+            <FormControlLabel control={<Checkbox />} label={<Rating name="5stars" value={5} readOnly />} />
+            <FormControlLabel control={<Checkbox />} label={<Rating name="4stars" value={4} readOnly />} />
+            <FormControlLabel control={<Checkbox />} label={<Rating name="3stars" value={3} readOnly />} />
+            <FormControlLabel control={<Checkbox />} label={<Rating name="2stars" value={2} readOnly />} />
+            <FormControlLabel control={<Checkbox />} label={<Rating name="1stars" value={1} readOnly />} />
+          </FormGroup>
         </Container>
       </Box>
     </Stack>
@@ -29,12 +34,23 @@ function ResultList() {
   const numberOfTestResults = 6;
   const results = [];
   for (let i = 0; i < numberOfTestResults; i++) {
-    results.push(<Box className="h-56" bgcolor="green"></Box>);
+    results.push(<Box className="h-56" bgcolor="green">
+      
+    </Box>);
   }
   return(
     <Stack className="pt-4 w-full object-right" spacing={2}>
+      <Container className="h-56 border-solid border-black hover:bg-gray-200">
+        <Grid container spacing={1} columns={9}>
+          <Grid item xs={3}>
+            <Box className="w-full h-56" bgcolor="green"></Box>
+          </Grid>
+          <Grid item xs={6}>
+            
+          </Grid>
+        </Grid>
+      </Container>
       {results}
-      <Box className="h-56" bgcolor="green"></Box>
     </Stack>
   );
 }
