d5998e8d5785aeaee0ec17701d7a95a04f460368
Seah Ying Xiang <seahyx@gmail.com>
2023-08-01 11:11:38 +0800

    Added bottom margin to prevent UI overlap

--- src/components/TopBarWithSearch.tsx
+++ src/components/TopBarWithSearch.tsx
@@ -3,7 +3,7 @@ import SearchBar from "./SearchBar";
 
 export default function TopBarWithSearch() {
   return (
-    <Box className="relative h-32 w-full" bgcolor="secondary.main">
+    <Box className="relative mb-6 h-32 w-full" bgcolor="secondary.main">
       <Container
         maxWidth="md"
         className="absolute -bottom-6 left-0 right-0 z-20"
