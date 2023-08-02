df8dee155ee25e21a7512de7a76c192443969284
Kendrick <kendricknghy@gmail.com>
2023-08-02 15:36:49 +0800

    Complete draft of profile page

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,12 +1,4 @@
-import {
-  Box,
-  Button,
-  Container,
-  Paper,
-  Stack,
-  TextField,
-  Typography,
-} from "@mui/material";
+import { Box, Button, Paper, Stack, Grid, Typography } from "@mui/material";
 import Head from "next/head";
 import Link from "next/link";
 
@@ -21,46 +13,34 @@ export default function Profile() {
         <meta name="description" content="View profile information." />
       </Head>
       <Box className="absolute h-32 w-full" bgcolor="secondary.main"></Box>
-      <Container
-        maxWidth="lg"
-        className="z-10 flex grow flex-col place-content-center items-center"
+      <Grid
+        className="mt-40"
+        container
+        direction="row"
+        justifyContent="space-around"
+        alignItems="flex-start"
       >
-        <Paper className="w-96 max-w-full place-content-center p-6">
-          <Stack direction={"column"} spacing={2}>
+        <Grid item>
+          <Paper className="h-auto w-80 p-4">
             <Typography
               component="h1"
               variant="h5"
               align="center"
-              className="pb-4 pt-3 font-semibold"
+              className="pb-4 font-semibold"
             >
               Profile
             </Typography>
-            <Container className="flex h-14 w-auto flex-row content-center">
-              <Typography className="pt-3 align-middle">First Name</Typography>
-              <TextField
-                id="first-name"
-                defaultValue={firstName}
-                required
-              ></TextField>
-            </Container>
-            <Container className="flex h-14 w-auto flex-row content-center">
-              <Typography className="pt-3 align-middle">Last Name</Typography>
-              <TextField
-                id="last-name"
-                defaultValue={lastName}
-                required
-              ></TextField>
-            </Container>
-            <Container className="flex h-14 w-auto flex-row content-center">
-              <Typography className="pr-1 pt-3 align-middle">Email</Typography>
-              <TextField id="email" defaultValue={email} required></TextField>
-            </Container>
-            <Container className="h-14 w-auto content-center">
-              <Button variant="contained">View Bookings</Button>
-            </Container>
-          </Stack>
-        </Paper>
-      </Container>
+            <Stack spacing={2}>
+              <Typography className="">First Name: {firstName}</Typography>
+              <Typography className="">Last Name: {lastName}</Typography>
+              <Typography className="">Email: {email}</Typography>
+            </Stack>
+          </Paper>
+        </Grid>
+        <Grid item>
+          <Button variant="contained">View Bookings</Button>
+        </Grid>
+      </Grid>
     </>
   );
 }
