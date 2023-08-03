724c5c0a279a24029c3eb567f3cca903b9d16a0f
Kendrick <kendricknghy@gmail.com>
2023-08-03 11:59:00 +0800

    Frontend for BookingHistory complete

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -4,20 +4,35 @@ import Link from "next/link";
 
 function BookingPreview() {
   return (
-    <Box className="flex h-56 w-auto flex-col bg-orange-200">
-      <Box className="">Image1</Box>
-      <Container>
-        <Typography>Hotel Name</Typography>
-        <Typography>Hotel Address</Typography>
-      </Container>
-      <Box className="">Image2</Box>
-      <Container>
-        <Typography>Room Type</Typography>
-        <Typography>Brief Booking Details</Typography>
-      </Container>
-      <Button variant="contained">View Full Details</Button>
+    <Box className="flex h-64 w-auto flex-row bg-orange-200">
+      <Stack>
+        <Box className="h-36 w-72 grow bg-green-200">Image1</Box>
+        <Container className="h-auto py-1">
+          <Typography>Hotel Name</Typography>
+          <Typography>Hotel Address</Typography>
+        </Container>
+      </Stack>
+      <Stack>
+        <Box className="h-36 w-72 grow bg-green-300">Image2</Box>
+        <Container className="h-auto py-1">
+          <Typography>Room Type</Typography>
+          <Typography>Brief Booking Details</Typography>
+        </Container>
+      </Stack>
+      <Button variant="contained" className="w-24">
+        View Full Details
+      </Button>
     </Box>
   );
+}
+
+function BookingList() {
+  const numberOfBookings = 4;
+  const results = [];
+  for (let i = 0; i < numberOfBookings; i++) {
+    results.push(<BookingPreview />);
+  }
+  return <>{results}</>;
 }
 
 export default function BookingHistory() {
@@ -28,8 +43,9 @@ export default function BookingHistory() {
         <meta name="description" content="View profile information." />
       </Head>
       <Box className="absolute h-32 w-full" bgcolor="secondary.main"></Box>
-      <Stack className="mt-40" spacing={2}>
+      <Stack className="mb-5 mt-40" spacing={6}>
         <BookingPreview />
+        <BookingList />
       </Stack>
     </>
   );
