5855ae28f27b86d6f86356850d2fa16757c2b169
Sherin Saji <sherinkaruvallilsaji@gmail.com>
2023-08-03 11:28:29 +0800

    commented out hydration error causing code

--- src/pages/details.tsx
+++ src/pages/details.tsx
@@ -16,7 +16,7 @@ function TrustYouBox() {
   return (
     <Box className="my-10 flex h-64 w-10/12 bg-orange-100">
       <Container className="w-2/3 p-2">
-        <Typography>Hotal Overview</Typography>
+        <Typography>Trust You Box</Typography>
       </Container>
       <Container></Container>
     </Box>
@@ -28,16 +28,17 @@ interface OverviewBoxProps {
 }
 
 function OverviewBox({ description }: OverviewBoxProps) {
-  const paragraphs = description
+  /*const paragraphs = description
     .split("</p>")
     .map((paragraph, index) => (
       <Typography key={index}>{parse(paragraph + "</p>")}</Typography>
-    ));
+    ));*/
 
   return (
     <Box className="my-10 h-auto w-10/12 bg-gray-200">
       <Typography>Hotel overview</Typography>
-      {paragraphs}
+      <Typography>{description}</Typography>
+      {/*{paragraphs}*/}
     </Box>
   );
 }
@@ -56,8 +57,8 @@ function Location() {
 
 export default function SearchResults() {
   const scrollToRooms = useRef();
-  const description =
-    "<p><b>Property Location</b> <br />With a stay at The Fullerton Hotel Singapore, you'll be centrally located in Singapore, steps from Cavenagh Bridge and Anderson Bridge.  This 5-star hotel is close to Chinatown Heritage Center and <b>Universal Studios Singapore</b>®.</p><p><b>Rooms</b> <br />Make yourself at home in one of the 400 individually furnished guestrooms, featuring refrigerators and plasma televisions. Complimentary wired and wireless Internet access keeps you connected, and satellite programming provides entertainment. Private bathrooms with separate bathtubs and showers feature deep soaking bathtubs and complimentary toiletries. Conveniences include phones, as well as laptop-compatible safes and desks.</p><p><b>Amenities</b> <br />Pamper yourself with a visit to the spa, which offers massages, body treatments, and facials. If you're looking for recreational opportunities, you'll find an outdoor pool and a fitness center. This Colonial hotel also features complimentary wireless Internet access, concierge services, and gift shops/newsstands. Guests can get to nearby shops on the complimentary shuttle.</p><p><b>Dining</b> <br />Grab a bite at one of the hotel's 5 restaurants, or stay in and take advantage of 24-hour room service. Quench your thirst with your favorite drink at a bar/lounge. Buffet breakfasts are available for a fee.</p><p><b>Business, Other Amenities</b> <br />Featured amenities include complimentary high-speed (wired) Internet access, a 24-hour business center, and limo/town car service. Planning an event in Singapore? This hotel has 7524 square feet (699 square meters) of space consisting of a conference center and meeting rooms. A roundtrip airport shuttle is provided for a surcharge (available 24 hours), and free self parking is available onsite.</p>";
+  const description = "Description";
+  /*"<p><b>Property Location</b> <br />With a stay at The Fullerton Hotel Singapore, you'll be centrally located in Singapore, steps from Cavenagh Bridge and Anderson Bridge.  This 5-star hotel is close to Chinatown Heritage Center and <b>Universal Studios Singapore</b>®.</p><p><b>Rooms</b> <br />Make yourself at home in one of the 400 individually furnished guestrooms, featuring refrigerators and plasma televisions. Complimentary wired and wireless Internet access keeps you connected, and satellite programming provides entertainment. Private bathrooms with separate bathtubs and showers feature deep soaking bathtubs and complimentary toiletries. Conveniences include phones, as well as laptop-compatible safes and desks.</p><p><b>Amenities</b> <br />Pamper yourself with a visit to the spa, which offers massages, body treatments, and facials. If you're looking for recreational opportunities, you'll find an outdoor pool and a fitness center. This Colonial hotel also features complimentary wireless Internet access, concierge services, and gift shops/newsstands. Guests can get to nearby shops on the complimentary shuttle.</p><p><b>Dining</b> <br />Grab a bite at one of the hotel's 5 restaurants, or stay in and take advantage of 24-hour room service. Quench your thirst with your favorite drink at a bar/lounge. Buffet breakfasts are available for a fee.</p><p><b>Business, Other Amenities</b> <br />Featured amenities include complimentary high-speed (wired) Internet access, a 24-hour business center, and limo/town car service. Planning an event in Singapore? This hotel has 7524 square feet (699 square meters) of space consisting of a conference center and meeting rooms. A roundtrip airport shuttle is provided for a surcharge (available 24 hours), and free self parking is available onsite.</p>";*/
   return (
     <>
       <Head>
