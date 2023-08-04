27afa7ec1987f606bf78fbe3837fbffdbd41baf9
Seah Ying Xiang <seahyx@gmail.com>
2023-08-04 15:54:37 +0800

    Removed unused components

--- src/components/SeeRoomOptionsBtn.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from "@mui/material";
-
-<Button variant="contained" 
-href="#contained-buttons" 
-color= "secondary" 
-onClick={()=>{alert('clicked')}}>
-  See room options
-</Button>
