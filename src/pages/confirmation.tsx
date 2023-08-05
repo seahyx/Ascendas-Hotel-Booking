5e73b8f6939648862e2d804c7fddaeb13aad5b07
zac_Low <zachary.l@gamelogic.com.sg>
2023-08-05 17:07:54 +0800

    Added Accordion for the Extra Info

--- src/pages/confirmation.tsx
+++ src/pages/confirmation.tsx
@@ -15,6 +15,7 @@ import {
 import Head from "next/head";
 import Image from "next/image";
 import BookingSummary from "~/components/BookingSummary";
+import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
 import { Convert, Hotels } from "~/server/api/idHotel";
 // const hotels = Convert.toHotels(json);
 
@@ -41,8 +42,8 @@ export default function Confirmation() {
   return (
     <>
       <Head>
-        <title>SUTDHotelBooking - Login</title>
-        <meta name="description" content="Login or register a new account." />
+        <title>SUTDHotelBooking - Booking Confirmation</title>
+        <meta name="description" content="Booking Confirmation" />
       </Head>
       <Box className="h-32 w-full" bgcolor="secondary.main"></Box>
       <Container className="mt-4 w-full">
@@ -72,67 +73,48 @@ export default function Confirmation() {
             <BookingSummary {...bookingData} />
           </Box>
 
-          {/*<Card className="mt-6 w-8/12 border-gray-300" variant="outlined">
-            <Typography
-              className="ml-3 mt-1 font-semibold underline"
-              component="h3"
-              variant="h5"
-            >
-              Booking Details
-            </Typography>
-            <Typography className="ml-3 text-base/loose">
-              Booking ID: 123456789
-              <br />
-              Room Type: Deluxe Room with Queen-size Beds
-              <br />
-              Check-in Date/Time: 23/4/2023 16:00
-              <br />
-              Check-out Date/Time: 24/4/2023 12:00
-              <br />
-              Number of Adults/Children: 2/0
-            </Typography>
-            <Typography className="ml-3 mt-5 text-xl font-semibold">
-              Total Price: $200
-            </Typography>
-  </Card>*/}
           <Box style={{ flex: 1 }}>
-            <Card className="mt-6 mb-6 w-full border-gray-300" variant='outlined'>
-              <CardHeader
-                title="Additional Information"
-                titleTypographyProps={{ variant: "h6" }}
-              />
-              <CardContent>
-              <Typography className="mt-2 text-lg underline">
-                Cancellation Policy
-              </Typography>
-              <Typography className="text-sm">
-                This booking is non-refundable from 22/4/2023, 16:00 onwards.
-                Cancellation before 22/4/2023 16:00 will incur a 50%
-                cancellation fee. All times indicated are in the GTC +8
-                timezone. Regardless of the cancellation policy, a 10%
-                cancellation fee of the total refundable amount of the booking
-                will be charged.
-              </Typography>
-              <Typography className="mt-2 text-lg underline">
-                Amendment Policy
-              </Typography>
-              <Typography className="text-sm">
-                To amend your booking in any way, you will have to cancel your
-                current reservation booking subject to the current cancellation
-                policy before making a new booking based on the prevailing rates
-                and availability.
-              </Typography>
-              <Typography className="mt-2 text-lg underline">
-                Further Information
-              </Typography>
-              <Typography className="mb-5 text-sm">
-                Club benefits are not included in this booking. If you would
-                like to add on any club benefits, please contact the hotel
-                directly at hotel@hotel.com.
-              </Typography>
-              </CardContent>
-            </Card>
+            <Accordion className="mt-6 mb-6 w-full border-gray-300" variant="outlined">
+              <AccordionSummary>
+                <Typography variant="h6">Cancellation Policy</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography variant="body1">
+                  This booking is non-refundable from 22/4/2023, 16:00 onwards. Cancellation
+                  before 22/4/2023 16:00 will incur a 50% cancellation fee. All times indicated
+                  are in the GTC +8 timezone. Regardless of the cancellation policy, a 10%
+                  cancellation fee of the total refundable amount of the booking will be
+                  charged.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+
+            <Accordion className="mt-6 mb-6 w-full border-gray-300" variant="outlined">
+              <AccordionSummary>
+                <Typography variant="h6">Amendment Policy</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography variant="body1">
+                  To amend your booking in any way, you will have to cancel your current
+                  reservation booking subject to the current cancellation policy before making
+                  a new booking based on the prevailing rates and availability.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+
+            <Accordion className="mt-6 mb-6 w-full border-gray-300" variant="outlined">
+              <AccordionSummary>
+                <Typography variant="h6">Further Information</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography variant="body1">
+                  Club benefits are not included in this booking. If you would like to add on
+                  any club benefits, please contact the hotel directly at hotel@hotel.com.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
           </Box>
+
         
       </Container>
     </>
