e1d95a155dea18b42cde64094c35ebad39a393b2
swarangim <124518209+swarangim@users.noreply.github.com>
2023-08-06 20:13:19 +0800

    fixed fuzz tests for login & register

--- cypress/e2e/login-function.cy.ts
+++ cypress/e2e/login-function.cy.ts
@@ -2,6 +2,16 @@
 // @ts-check
 
 describe('Login', () => {
+
+  function generateRandomString(length: number) {
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      result += characters[randomIndex];
+    }
+    return result;
+  }
     it('should allow input & button click', () => {
       cy.visit('http://localhost:3000/login')
       cy.get('input[name="email"]').type("abc@example.com")
@@ -10,10 +20,10 @@ describe('Login', () => {
     })
     it('should NOT allow log in if invalid email or password', () => {
         cy.visit('http://localhost:3000/login')
-        cy.get('input[name="email"]').type("abc@example.com")
-        cy.get('input[name="password"]').type("Password1")
+        cy.get('input[name="email"]').type(generateRandomString(10) + "@example.com")
+        cy.get('input[name="password"]').type(generateRandomString(10))
         cy.get("button").click() 
-        cy.url().should('include', '/login')
+        cy.url().should('include', 'error')
       })
     it('should allow log in (& redirect to main) if correct email and password', () => {
         cy.visit('http://localhost:3000/login')
@@ -22,24 +32,12 @@ describe('Login', () => {
         cy.get("#submit").click()
         cy.url().should("not.include", "/login") 
       })
-
-    it('should not crash after fuzzing', () => {
-      function generateRandomString(length: number) {
-        const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-        let result = '';
-        for (let i = 0; i < length; i++) {
-          const randomIndex = Math.floor(Math.random() * characters.length);
-          result += characters[randomIndex];
-        }
-        return result;
-      }
-
-      cy.visit('http://localhost:3000/login')
-
-      for(let i = 0; i<100;i++){
+    for(let i = 0; i<20;i++){
+      it('should not crash after fuzzing, test:' + i, () => {
+        cy.visit('http://localhost:3000/login')
         cy.get('input[name="email"]').type(generateRandomString(10) + "@example.com")
         cy.get('input[name="password"]').type(generateRandomString(10))
         cy.get("#submit").click()
-      }
-    })
+      })
+    }
   })
--- cypress/e2e/registration-function.cy.ts
+++ cypress/e2e/registration-function.cy.ts
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 // @ts-check
 
-describe('Login', () => {
+describe('Registration', () => {
     it('should allow input & button click', () => {
       cy.visit('http://localhost:3000/register')
       cy.get('input[name="name"]').type("Guest")
@@ -28,12 +28,12 @@ describe('Login', () => {
         cy.get("#submit").click() //TO-DO 
         cy.url().should('include', '/register')
       })
-      it("should NOT allow registration if passwords don't match", () => {
+      it("should NOT allow registration if invalid confirm password", () => {
         cy.visit('http://localhost:3000/register')
         cy.get('input[name="name"]').type("Guest")
         cy.get('input[name="email"]').type("abc@example.com")
-        cy.get('input[name="password"]').type("Pasword1")
-        cy.get('input[name="confirmPassword"]').type("Password2")
+        cy.get('input[name="password"]').type("Password1")
+        cy.get('input[name="confirmPassword"]').type("P2")
         cy.get("#submit").click() //TO-DO 
         cy.url().should('include', '/register')
       })
@@ -54,28 +54,28 @@ describe('Login', () => {
         cy.get('input[name="password"]').type(pass)
         cy.get('input[name="confirmPassword"]').type(pass) 
         cy.get("#submit").click()  
-        cy.url().should("not.include", "/register")
+        cy.url().should("not.include", "error")
       })
 
-      it('should not crash after fuzzing', () => {
-        function generateRandomString(length: number) {
-          const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-          let result = '';
-          for (let i = 0; i < length; i++) {
-            const randomIndex = Math.floor(Math.random() * characters.length);
-            result += characters[randomIndex];
-          }
-          return result;
+      function generateRandomString(length: number) {
+        const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+        let result = '';
+        for (let i = 0; i < length; i++) {
+          const randomIndex = Math.floor(Math.random() * characters.length);
+          result += characters[randomIndex];
         }
-  
-        cy.visit('http://localhost:3000/register')
-  
-        for(let i = 0; i<100;i++){
+        return result;
+      }
+
+      for(let i = 0; i<20;i++){
+        it('should not crash after fuzzing, test:' + i, () => {
+          cy.visit('http://localhost:3000/register')
           cy.get('input[name="name"]').type(generateRandomString(10))
           cy.get('input[name="email"]').type(generateRandomString(10) + "@example.com")
-          cy.get('input[name="password"]').type(generateRandomString(10))
-          cy.get('input[name="confirmPassword"]').type(generateRandomString(10))
+          let pass = generateRandomString(10)
+          cy.get('input[name="password"]').type(pass)
+          cy.get('input[name="confirmPassword"]').type(pass)
           cy.get("#submit").click() //TO-DO
-        }
-      })
+        })
+      }
   })
