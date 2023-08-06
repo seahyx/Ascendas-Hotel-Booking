b17edc4a637a0c549c724a9bd69a69e16fd70485
Eqedos <parthkumarj117@gmail.com>
2023-08-06 14:09:55 +0800

    Added booking functions

---
+++ prisma/migrations/20230805061939_hotelbooking/migration.sql
@@ -0,0 +1,65 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `bookingInfoId` on the `booking` table. All the data in the column will be lost.
+  - You are about to drop the column `guestInfoId` on the `booking` table. All the data in the column will be lost.
+  - You are about to drop the column `payeeInfoId` on the `booking` table. All the data in the column will be lost.
+  - You are about to drop the `bookingdisplayinformation` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `guestinformation` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `payeeinformation` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `adults` to the `Booking` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `avgRoomCost` to the `Booking` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `children` to the `Booking` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `email` to the `Booking` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `endDate` to the `Booking` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `firstName` to the `Booking` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `lastName` to the `Booking` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `messageToHotel` to the `Booking` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `numberOfNights` to the `Booking` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `numberOfRooms` to the `Booking` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `phoneNumber` to the `Booking` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `roomRate` to the `Booking` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `roomTypes` to the `Booking` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `startDate` to the `Booking` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `tax` to the `Booking` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `uid` to the `Booking` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE `booking` DROP FOREIGN KEY `Booking_bookingInfoId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `booking` DROP FOREIGN KEY `Booking_guestInfoId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `booking` DROP FOREIGN KEY `Booking_payeeInfoId_fkey`;
+
+-- AlterTable
+ALTER TABLE `booking` DROP COLUMN `bookingInfoId`,
+    DROP COLUMN `guestInfoId`,
+    DROP COLUMN `payeeInfoId`,
+    ADD COLUMN `adults` INTEGER NOT NULL,
+    ADD COLUMN `avgRoomCost` DOUBLE NOT NULL,
+    ADD COLUMN `children` INTEGER NOT NULL,
+    ADD COLUMN `email` VARCHAR(191) NOT NULL,
+    ADD COLUMN `endDate` DATETIME(3) NOT NULL,
+    ADD COLUMN `firstName` VARCHAR(191) NOT NULL,
+    ADD COLUMN `lastName` VARCHAR(191) NOT NULL,
+    ADD COLUMN `messageToHotel` VARCHAR(191) NOT NULL,
+    ADD COLUMN `numberOfNights` INTEGER NOT NULL,
+    ADD COLUMN `numberOfRooms` INTEGER NOT NULL,
+    ADD COLUMN `phoneNumber` VARCHAR(191) NOT NULL,
+    ADD COLUMN `roomRate` DOUBLE NOT NULL,
+    ADD COLUMN `roomTypes` VARCHAR(191) NOT NULL,
+    ADD COLUMN `startDate` DATETIME(3) NOT NULL,
+    ADD COLUMN `tax` DOUBLE NOT NULL,
+    ADD COLUMN `uid` INTEGER NOT NULL;
+
+-- DropTable
+DROP TABLE `bookingdisplayinformation`;
+
+-- DropTable
+DROP TABLE `guestinformation`;
+
+-- DropTable
+DROP TABLE `payeeinformation`;
---
+++ src/server/api/bookingfunctions.ts
@@ -0,0 +1,107 @@
+import { PrismaClient, Booking } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+// Function to add a new booking
+async function addBooking(
+  destinationId: number,
+  hotelId: number,
+  uid: number,
+  firstName: string,
+  lastName: string,
+  phoneNumber: string,
+  email: string,
+  numberOfNights: number,
+  numberOfRooms: number,
+  startDate: Date,
+  endDate: Date,
+  adults: number,
+  children: number,
+  messageToHotel: string,
+  roomTypes: string,
+  avgRoomCost: number,
+  roomRate: number,
+  tax: number,
+  additionalInfo: string
+): Promise<Booking> {
+  const newBooking = await prisma.booking.create({
+    data: {
+      destinationId,
+      hotelId,
+      uid,
+      firstName,
+      lastName,
+      phoneNumber,
+      email,
+      numberOfNights,
+      numberOfRooms,
+      startDate,
+      endDate,
+      adults,
+      children,
+      messageToHotel,
+      roomTypes,
+      avgRoomCost,
+      roomRate,
+      tax,
+      additionalInfo,
+    },
+  });
+
+  return newBooking;
+}
+
+async function getBookings(uidn: number) {
+  try {
+    const res = await prisma.booking.findMany({
+      where: {
+        uid: {
+          equals: uidn, // 
+        },
+      },
+    });
+
+    console.log(res);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await prisma.$disconnect(); // Don't use an async function in the finally block, directly await the $disconnect()
+  }
+}
+
+async function run() {
+  try {
+    const newBooking = await addBooking(
+      1, // destinationId
+      2, // hotelId
+      3, // uid
+      'John', // firstName
+      'Doe', // lastName
+      '123456789', // phoneNumber
+      'john.doe@example.com', // email
+      3, // numberOfNights
+      2, // numberOfRooms
+      new Date(), // startDate
+      new Date(), // endDate
+      2, // adults
+      1, // children
+      'Some message', // messageToHotel
+      'Double, Single', // roomTypes
+      120.0, // avgRoomCost
+      50.0, // roomRate
+      10.0, // tax
+      'Additional info' // additionalInfo
+    );
+
+    console.log('New booking:', newBooking);
+
+    const bookings = await getBookings(3);
+
+    console.log('Read Booking', bookings)
+  } catch (error) {
+    console.error('Error creating booking:', error);
+  }
+}
+
+run();
+export { addBooking, getBookings };
